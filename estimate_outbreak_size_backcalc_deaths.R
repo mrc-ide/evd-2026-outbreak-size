fafa52a48142b112721a54fb304e9039c744f540
ruthmccabe <ruth.mccabe17@imperial.ac.uk>
2026-05-22 00:25:57 +0200

    tidied functions for both methods

---
+++ estimate_outbreak_size_backcalc_deaths.R
@@ -0,0 +1,68 @@
+## implement Neil method
+
+# ttd_mean <- 11.37
+# ttd_sd <- 5.41
+# 
+# r <- 0.1
+# deaths <- 88
+# cfr <- 0.24
+# k <- 1000000
+
+estimate_outbreak_size_backcalc_deaths <- function(ttd_mean,
+                                                   ttd_sd,
+                                                   cfr,
+                                                   deaths,
+                                                   r,
+                                                   k){
+  doubling_time <- log(2)/r
+
+  alpha <- epitrix::gamma_mucv2shapescale(mu = ttd_mean, cv = ttd_sd / ttd_mean)$shape
+  beta <- 1/epitrix::gamma_mucv2shapescale(mu = ttd_mean, cv = ttd_sd / ttd_mean)$scale
+  
+  # raw estimate
+  Nt <- (deaths/cfr) * (1 + (r/beta))^alpha
+  
+  # add CIs
+  #Fixing r and then looping through values of T between 10 and 250 on a fine (0.02) grid to give required likelihood profile
+  t_max_grid <- seq(10,250,0.02)
+  
+  results <- data.frame("t_max" = t_max_grid) %>%
+    mutate(Nt_max = exp(r*t_max_grid),
+           Dt_max = Nt_max * cfr / (1 + r/beta)^alpha,
+           llt_max = lgamma(k + deaths) - lgamma(1 + deaths) - lgamma(k) +
+             k * log(k / (k + Dt_max)) + deaths * log(Dt_max / (k + Dt_max)))
+
+    ## bounds
+    threshold <- max(results$llt_max)-1.92
+
+    results_with_CIs <- results %>% mutate(
+      lower = case_when(
+        llt_max < threshold & lead(llt_max) > threshold ~ 1,
+        .default = 0
+      ),
+      upper = case_when(
+        llt_max > threshold & lead(llt_max) < threshold ~ 1,
+        .default = 0
+      )
+    ) 
+
+  return(data.frame("ttd_mean" = ttd_mean,
+                    "ttd_sd" = ttd_sd,
+                    "cfr" = cfr,
+                    "deaths" = deaths,
+                    "r" = r,
+                    "doubling_time" = doubling_time,
+                    "Nt" = Nt,
+                    "N_central" = results_with_CIs %>% filter(llt_max==max(llt_max)) %>% 
+                      select(Nt_max) %>% rename(N_central = Nt_max), 
+                    "N_lower" = results_with_CIs %>% filter(lower==1) %>% 
+                      select(Nt_max) %>% rename(N_lower = Nt_max),
+                    "N_upper" = results_with_CIs %>% filter(upper==1) %>% 
+                      select(Nt_max) %>% rename(N_upper = Nt_max)))
+}
+
+
+
+
+
+
